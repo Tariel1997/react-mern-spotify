76a59ec5aa3a169e244af0221cdea01e0102a71d
Tariel Khalvashi <taro.khalvashi1225@gmail.com>
2026-03-09 22:21:30 +0400

    first commit

---
+++ eslint.config.js
@@ -0,0 +1,69 @@
+import js from '@eslint/js'
+import prettierConfig from 'eslint-config-prettier'
+import prettierPlugin from 'eslint-plugin-prettier'
+import pluginReact from 'eslint-plugin-react'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import { defineConfig } from 'eslint/config'
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
+
+export default defineConfig([
+  {
+    ignores: [
+      'node_modules',
+      'dist',
+      'frontend/dist',
+      'backend/dist',
+      '.env',
+      '.git',
+      '.vscode',
+      '**/*.html',
+    ],
+  },
+
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+
+  {
+    files: ['**/*.{js,mjs,cjs,jsx,ts,tsx}'],
+    languageOptions: {
+      parser: tseslint.parser,
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+      parserOptions: {
+        ecmaFeatures: { jsx: true },
+      },
+    },
+  },
+  {
+    files: ['frontend/src/**/*.{js,jsx,ts,tsx}'],
+    plugins: {
+      react: pluginReact,
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
+    settings: {
+      react: { version: 'detect' },
+    },
+    rules: {
+      ...pluginReact.configs.flat.recommended.rules,
+      ...reactHooks.configs.recommended.rules,
+      'react/prop-types': 'off',
+      'react/react-in-jsx-scope': 'off',
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+    },
+  },
+  {
+    plugins: { prettier: prettierPlugin },
+    rules: {
+      // 'prettier/prettier': 'warn',
+    },
+  },
+  prettierConfig,
+])
