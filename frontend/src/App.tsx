700f85921d87172a9795e2b547a400556cda2c18
Tariel Khalvashi <taro.khalvashi1225@gmail.com>
2026-03-17 02:27:08 +0400

    feat: implement LeftSidebar with navigation and playlist skeleton

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,15 @@
 import { AuthenticateWithRedirectCallback } from '@clerk/react'
 import { Route, Routes } from 'react-router-dom'
 
+import MainLayout from './layout/MainLayout'
 import AuthCallbackPage from './pages/auth-callback/AuthCallbackPage'
+import ChatPage from './pages/chat/ChatPage'
 import HomePage from './pages/home/HomePage'
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<HomePage />} />
         <Route
           path="/sso-callback"
           element={
@@ -18,6 +19,11 @@ function App() {
           }
         />
         <Route path="/auth-callback" element={<AuthCallbackPage />} />
+
+        <Route element={<MainLayout />}>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/chat" element={<ChatPage />} />
+        </Route>
       </Routes>
     </>
   )
