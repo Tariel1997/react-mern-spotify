1d3ea9e7c26b1549eb10681b7c4d4a35035a8fda
Tariel Khalvashi <taro.khalvashi1225@gmail.com>
2026-03-17 23:20:19 +0400

    feat: implement album fetching with Zustand and display in LeftSidebar

--- frontend/src/layout/components/LeftSidebar.tsx
+++ frontend/src/layout/components/LeftSidebar.tsx
@@ -2,12 +2,20 @@ import PlaylistSkeleton from '@/components/skeletons/PlaylistSkeleton'
 import { buttonVariants } from '@/components/ui/button'
 import { ScrollArea } from '@/components/ui/scroll-area'
 import { cn } from '@/lib/utils'
+import { useMusicStore } from '@/stores/useMusicStore.ts'
 import { Show } from '@clerk/react'
 import { HomeIcon, Library, MessageCircle } from 'lucide-react'
+import { useEffect } from 'react'
 import { Link } from 'react-router-dom'
 
 const LeftSidebar = () => {
-  const isLoading = false
+  const { albums, fetchAlbums, isLoading } = useMusicStore()
+
+  useEffect(() => {
+    fetchAlbums()
+  }, [fetchAlbums])
+
+  console.log({ albums })
 
   return (
     <div className="h-full flex flex-col gap-2">
@@ -46,7 +54,7 @@ const LeftSidebar = () => {
       </div>
 
       {/* Library section */}
-      <div className="flex-1 bg-zinc-900 p-4">
+      <div className="flex-1 rounded-lg bg-zinc-900 p-4">
         <div className="flex items-center justify-between mb-4">
           <div className="flex items-center text-white px-2">
             <Library className="size-5 mr-2" />
@@ -56,7 +64,30 @@ const LeftSidebar = () => {
 
         <ScrollArea className="h-[calc(100vh-300px)]">
           <div className="space-y-2">
-            {isLoading ? <PlaylistSkeleton /> : 'Some music'}
+            {isLoading ? (
+              <PlaylistSkeleton />
+            ) : (
+              albums?.map((album) => (
+                <Link
+                  to={`/albums/${album._id}`}
+                  key={album._id}
+                  className="p-2 hover:bg-zinc-800 rounded-md flex items-center gap-3 group cursor-pointer"
+                >
+                  <img
+                    src={album.imageUrl}
+                    alt="Playlist image"
+                    className={'size-12 rounded-md flex-shrink-0 object-cover'}
+                  />
+
+                  <div className={'flex-1 min-w-0 hidden md:block'}>
+                    <p className={'font-medium truncate'}>{album.title}</p>
+                    <p className={'text-sm text-zinc-400 truncate'}>
+                      Album • {album.artist}
+                    </p>
+                  </div>
+                </Link>
+              ))
+            )}
           </div>
         </ScrollArea>
       </div>
