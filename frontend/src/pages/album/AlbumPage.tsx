ea5f4666a1b02a0f8128218155ee6402b493d1e0
Tariel Khalvashi <taro.khalvashi1225@gmail.com>
2026-03-18 17:17:53 +0400

    feat: implement album tracklist table with duration formatting and hover states

--- frontend/src/pages/album/AlbumPage.tsx
+++ frontend/src/pages/album/AlbumPage.tsx
@@ -1,7 +1,15 @@
+import { Button } from '@/components/ui/button.tsx'
 import { ScrollArea } from '@/components/ui/scroll-area.tsx'
 import { useMusicStore } from '@/stores/useMusicStore.ts'
+import { Clock, Play } from 'lucide-react'
 import { useEffect } from 'react'
 import { useParams } from 'react-router-dom'
+
+const formatDuration = (seconds: number) => {
+  const minutes = Math.floor(seconds / 60)
+  const remainingSeconds = seconds % 60
+  return `${minutes}:${remainingSeconds.toString().padStart(2, '0')}`
+}
 
 const AlbumPage = () => {
   const { albumId } = useParams()
@@ -42,6 +50,68 @@ const AlbumPage = () => {
                   </span>
                   <span>• {currentAlbum?.songs.length} songs</span>
                   <span>• {currentAlbum?.releaseYear}</span>
+                </div>
+              </div>
+            </div>
+
+            {/*Play Button*/}
+            <div className="px-6 pb-4 flex items-center gap-6">
+              <Button
+                size="icon"
+                className="w-14 h-14 rounded-full bg-green-500 hover:bg-green-400 hover:scale-105 transition-all"
+              >
+                <Play className="h-7 w-7 text-black" />
+              </Button>
+            </div>
+
+            {/*Table Section*/}
+            <div className={'bg-black/20 backdrop-blur-sm'}>
+              {/*Table header*/}
+              <div className="grid grid-cols-[16px_4fr_2fr_1fr] gap-4 px-10 py-2 text-sm text-zinc-400 border-b border-white/5">
+                <div>#</div>
+                <div>Title</div>
+                <div>Released Date</div>
+                <div>
+                  <Clock className="h-4 w-4" />
+                </div>
+              </div>
+
+              {/*Songs List*/}
+              <div className={'p-6'}>
+                <div className={'space-y-2 py-4'}>
+                  {currentAlbum?.songs.map((song, index) => (
+                    <div
+                      key={song._id}
+                      className={`grid grid-cols-[16px_4fr_2fr_1fr] gap-4 px-4 py-2 text-sm  
+                      text-zinc-400 hover:bg-white/5 rounded-md group cursor-pointer`}
+                    >
+                      <div className={'flex items-center justify-center'}>
+                        <span className={'group-hover:hidden'}>{index + 1}</span>
+                        <Play className="h-4 w-4 hidden group-hover:block" />
+                      </div>
+
+                      <div className={'flex items-center gap-3'}>
+                        <img
+                          src={song.imageUrl}
+                          alt={song.title}
+                          className={'size-10'}
+                        />
+
+                        <div>
+                          <div className={`font-medium text-white`}>
+                            {song.title}
+                          </div>
+                          <div>{song.artist}</div>
+                        </div>
+                      </div>
+                      <div className="flex items-center">
+                        {song.createdAt.split('T')[0]}
+                      </div>
+                      <div className={'flex items-center'}>
+                        {formatDuration(song.duration)}
+                      </div>
+                    </div>
+                  ))}
                 </div>
               </div>
             </div>
