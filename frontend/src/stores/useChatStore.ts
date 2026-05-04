cd81aff523e09b743607c0722251e3a220835b07
Tariel Khalvashi <taro.khalvashi1225@gmail.com>
2026-05-04 17:27:24 +0400

    feat: add fetchMessages and selectedUser actions to chat store and replace 'any' with proper types

--- frontend/src/stores/useChatStore.ts
+++ frontend/src/stores/useChatStore.ts
@@ -1,18 +1,47 @@
 import { axiosInstance } from '@/lib/axios.ts'
+import { Message, User } from '@/types'
 import { AxiosError } from 'axios'
+import { io, Socket } from 'socket.io-client'
 import { create } from 'zustand'
 
 interface ChatStore {
-  users: any[]
-  fetchUsers: () => Promise<void>
+  users: User[]
   isLoading: boolean
   error: string | null
+  socket: Socket | null
+  isConnected: boolean
+  onlineUsers: Set<string>
+  userActivities: Map<string, string>
+  messages: Message[]
+  selectedUser: User | null
+
+  fetchUsers: () => Promise<void>
+  initSocket: (_userId: string) => void
+  disconnectSocket: () => void
+  sendMessage: (_receiverId: string, _senderId: string, _content: string) => void
+  fetchMessages: (_userId: string) => Promise<void>
+  setSelectedUser: (_user: User | null) => void
 }
+
+const baseURL = 'http://localhost:5001'
+
+const socket = io(baseURL, {
+  autoConnect: false, // Only connect if user is authenticated
+  withCredentials: true,
+})
 
 export const useChatStore = create<ChatStore>((set, get) => ({
   users: [],
   isLoading: false,
   error: null,
+  socket: null,
+  isConnected: false,
+  onlineUsers: new Set(),
+  userActivities: new Map(),
+  messages: [],
+  selectedUser: null,
+
+  setSelectedUser: (user) => set({ selectedUser: user }),
 
   fetchUsers: async () => {
     if (get().users.length > 0 || get().isLoading) return
@@ -32,5 +61,87 @@ export const useChatStore = create<ChatStore>((set, get) => ({
     }
 
     console.timeEnd('Network request')
+  },
+
+  initSocket: (userId: string) => {
+    if (!get().isConnected) {
+      socket.auth = { userId }
+      socket.connect()
+      socket.emit('user_connected', userId)
+
+      socket.on('user_online', (users: string[]) => {
+        set({ onlineUsers: new Set(users) })
+      })
+
+      socket.on('activities', (activities: [string, string][]) => {
+        set({ userActivities: new Map(activities) })
+      })
+
+      socket.on('user_connected', (userId: string) => {
+        set((state) => ({
+          onlineUsers: new Set([...state.onlineUsers, userId]),
+        }))
+      })
+
+      socket.on('user_disconnected', (userId: string) => {
+        set((state) => {
+          const newOnlineUsers = new Set(state.onlineUsers)
+          newOnlineUsers.delete(userId)
+          return { onlineUsers: newOnlineUsers }
+        })
+      })
+
+      socket.on('receive_message', (message: Message) => {
+        set((state) => ({
+          messages: [...state.messages, message],
+        }))
+      })
+
+      socket.on('message_sent', (message: Message) => {
+        set((state) => ({
+          messages: [...state.messages, message],
+        }))
+      })
+
+      socket.on('activity_updated', ({ userId, activity }) => {
+        set((state) => {
+          const newActivities = new Map(state.userActivities)
+          newActivities.set(userId, activity)
+          return { userActivities: newActivities }
+        })
+      })
+
+      set({ isConnected: true })
+    }
+  },
+
+  disconnectSocket: () => {
+    if (get().isConnected) {
+      socket.disconnect()
+      set({ isConnected: false })
+    }
+  },
+
+  sendMessage: async (receiverId, senderId, content) => {
+    const socket = get().socket
+    if (!socket) return
+
+    socket.emit('send_message', { receiverId, senderId, content })
+  },
+
+  fetchMessages: async (userId: string) => {
+    set({ isLoading: true, error: null })
+    try {
+      const response = await axiosInstance.get(`/users/messages/${userId}`)
+      set({ messages: response.data })
+    } catch (error: unknown) {
+      if (error instanceof AxiosError) {
+        set({ error: error.response?.data?.message })
+      } else {
+        set({ error: 'Generic error occurred' })
+      }
+    } finally {
+      set({ isLoading: false })
+    }
   },
 }))
